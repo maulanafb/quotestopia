7fce8997237d04854f739ba0fd69e4aaee2a2b92
unknown <gagas112212@gmail.com>
2023-09-21 16:04:11 +0700

    Fix Fetch vercel

--- app/api/quotes/route.js
+++ app/api/quotes/route.js
@@ -1,14 +1,15 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
+export const dynamic = 'force-dynamic';
 export const GET = async (request) => {
-    try {
-        await connectToDB();
-        const prompts = await Prompt.find({}).populate
-            ('creator');
-        
-        return new Response(JSON.stringify(prompts),{status:200});
-    } catch (error) {
-        return new Response('Failed to fetch a prompt', { status:404})
-    }
-}
+  try {
+    await connectToDB();
+
+    const prompts = await Prompt.find({}).populate('creator');
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+} 
